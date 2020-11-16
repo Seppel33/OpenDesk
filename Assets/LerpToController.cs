afa912e8f664719bd8a41f5e0e6ecf7bc6f97fc4
badir <badir.al-sayyed@stud-hshl.de>
2020-11-16 19:59:55 +0100

    LerpToController

---
+++ Assets/LerpToController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpToController : MonoBehaviour
+{
+    public GameObject rightController;
+    private GameObject table;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            gameObject.transform.position = rightController.transform.position;
+            gameObject.transform.position -= new Vector3(0, 0.07f, 0);
+        }
+    }
+}
